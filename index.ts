a57feea8231c7134068548642199e0e175f8d834
NoSkhil <nshyamakhil@gmail.com>
2024-06-26 03:47:12 -0400

    initialise

---
+++ index.ts
@@ -0,0 +1,27 @@
+import express,{Request, Response} from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors';
+//import userRoutes from './routes/emailRoutes';
+//import client from './db';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req:Request,res:Response)=> res.status(200).send({data:"12 server."}));
+//app.use('/api/email', emailRoutes);
+
+app.listen(8000, async ()=>{
+    console.log("12 server active on port 8000");
+    /*
+    client.connect((err:any)=>{
+        if(err) console.log(err);
+        else {
+            console.log("Connected to supabase postgres db");
+        }
+    });
+    */
+});
+
